752d54f74d87b098652489c2ab63ef542c279ae4
Jacob Kotzamanis <jkotz203@d127.org>
2022-10-24 09:26:10 -0500

    Initial Commit

---
+++ Clock and Organization App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clock and Organization App
+//
+//  Created by Jacob Kotzamanis (Student) on 10/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
