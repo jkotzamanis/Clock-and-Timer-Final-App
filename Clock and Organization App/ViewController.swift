a0f303a22028c651cf21bc676f664cbb034c2b33
Jacob Kotzamanis <jkotz203@d127.org>
2022-12-07 09:30:03 -0600

    Started new main screen

--- Clock and Organization App/ViewController.swift
+++ Clock and Organization App/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var backgroundColorLabel: UILabel!
+    
+    @IBOutlet weak var addCalenderEntryButtion: UIButton!
+    
+    @IBOutlet weak var addCalenderEntryLabel: UILabel!
+    
+    @IBOutlet weak var changeBackgroundButton: UIButton!
+    
+    @IBOutlet weak var changeBackgroundLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view
+        backgroundColorLabel.layer.cornerRadius = 20
+        addCalenderEntryButtion.layer.cornerRadius = 50
+        addCalenderEntryLabel.layer.cornerRadius = 15
+        changeBackgroundButton.layer.cornerRadius = 50
+        changeBackgroundLabel.layer.cornerRadius = 15
+        
+        
     }
-
-
 }
+
 
